fcee994cb6880eedeafa957ede9a640d3382bd07
ederAlonso <ederalonsoh@gmail.com>
2026-05-13 01:52:20 -0500

    Delete JUEGO-EXPLORADOR.n2 directory

--- JUEGO-EXPLORADOR.n2/scenes/obj/door.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-# puerta para pasar nivel
-
-signal siguiente
-
-func _on_body_entered(body):
-	if body.name == "Player" and body.has_key:
-		emit_signal("siguiente")
-		print("Nivel completado")
