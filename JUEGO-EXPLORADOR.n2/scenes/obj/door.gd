66f3c98a7edb63a6b66ace3f5a0f879bfd1e5cf8
ederAlonso <ederalonsoh@gmail.com>
2026-05-31 12:54:58 -0500

    implementacion de imagen de escena menu y final

--- JUEGO-EXPLORADOR.n2/scenes/obj/door.gd
+++ JUEGO-EXPLORADOR.n2/scenes/obj/door.gd
@@ -5,5 +5,5 @@ signal siguiente
 
 func _on_body_entered(body):
 	if body.name == "Player" and body.has_key:
-		emit_signal("siguiente")
+		get_tree().change_scene_to_file("res://scenes/levels/juego_terminado.tscn")
 		print("Nivel completado")
